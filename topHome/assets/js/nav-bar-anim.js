d7cdf2f20b6b400148b73a369a23fe983f069160
noara <tutoravacefet@gmail.com>
2022-12-18 23:52:12 -0300

    completed successfully

---
+++ topHome/assets/js/nav-bar-anim.js
@@ -0,0 +1,8 @@
+$('#bs-example-navbar-collapse-1').on('show.bs.collapse',function(){
+    $(".TopCasaFina-banner").css("transform","translate(-50%, 10%)");
+})
+
+$('#bs-example-navbar-collapse-1').on('hide.bs.collapse',function(){
+    $(".TopCasaFina-banner").css("transform","translate(-50%, -50%)");
+})
+
